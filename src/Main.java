151211c09b940265389e29bac8e2c034fc5165ab
annayrina <annayrina1998@mail.ru>
2023-03-02 01:22:38 +0700

    не могу понять ошибку

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("task1");
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для IOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+
+        }
+    }
+
+    public static void main(String[] args) {task2(); }
+
+    public static void task2() {
+        int clientOs = 0;
+        int clientDeviceYear = 2020;
+
+        if (clientOs = 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию для IOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для IOS по ссылке");
+            }
+        } else {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
     }
 }
+
+
+
+
+
